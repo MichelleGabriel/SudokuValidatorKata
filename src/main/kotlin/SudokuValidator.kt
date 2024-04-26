27d2f26795e22a1b6087b1a3228ee6e1f91b0d24
Michelle Gabriel <michelle.gabriel@volkswagen.de>
2024-04-26 11:27:29 +0200

    all rows are 1 - 9 solved

--- src/main/kotlin/SudokuValidator.kt
+++ src/main/kotlin/SudokuValidator.kt
@@ -1,4 +1,22 @@
 fun validate(board: Array<Array<Int>>): Boolean {
-    return board
-        .all { row -> row.size == row.distinct().size}
+    val allRows = board
+    val allColumns = board.columns()
+    val allColumnsDistinct = allColumns.isDistinct()
+    val allRowsDistinct = allRows.isDistinct()
+
+
+    return allRowsDistinct && allColumnsDistinct
 }
+
+private fun Array<Array<Int>>.isDistinct() =
+    this.all { it.size == it.distinct().size }
+
+private fun Array<Array<Int>>.column(index: Int) =
+    this.map { row -> row[index] }
+        .toTypedArray()
+
+private fun Array<Array<Int>>.columns() =
+    (0..<this.first().size)
+        .map { columIndex -> column(columIndex) }
+        .toTypedArray()
+
