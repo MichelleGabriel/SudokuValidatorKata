a8be2778143534b7b0ef69824edd6b2d7d4d5b4d
Michelle Gabriel <michelle.gabriel@volkswagen.de>
2024-04-29 11:27:46 +0200

    Solution from Trainer

--- src/main/kotlin/SudokuValidator.kt
+++ src/main/kotlin/SudokuValidator.kt
@@ -1,21 +1,23 @@
-fun validate(board: Array<Array<Int>>): Boolean {
-    val allRows = board
-    val allColumns = board.columns()
-    val allColumnsDistinct = allColumns.isValid()
-    val allRowsDistinct = allRows.isValid()
+fun validate(board: Array<Array<Int>>) = Board(board).isValid()
 
-    return allRowsDistinct && allColumnsDistinct
+data class Board(private val values: Array<Array<Int>>) {
+    fun isValid() = allBoxesValid() && allColumnsValid() && allRowsValid()
+    private fun allRowsValid() = this.values.all { it.isDistinct() }
+    private fun allColumnsValid() = allColumns().all { it.isDistinct() }
+    private fun allBoxesValid() = allBoxes().all { it.isDistinct() }
+    private fun allColumns() = (0..<this.values[0].size).map { column(it) }
+    private fun allBoxes() = (0..<this.values[0].size).map { box(it) }
+    private fun column(id: Int) = values.map { it[id] }.toTypedArray()
+    private fun box(id: Int) = createBox(id / 3 * 3, id % 3 * 3)
+    private fun createBox(startRow: Int, startColumn: Int) = (startRow..<startRow + 3)
+        .map { row -> this.values[row].sliceArray(startColumn..<3).toList() }
+        .flatten()
+        .toTypedArray()
 }
 
-private fun Array<Array<Int>>.isValid() =
-    this.all { it.toSet() == (1..9).toSet()}
+fun Array<Int>.isDistinct() = this.count() == this.distinctValidNumbers()
 
-private fun Array<Array<Int>>.column(index: Int) =
-    this.map { row -> row[index] }
-        .toTypedArray()
-
-private fun Array<Array<Int>>.columns() =
-    (0..<this.first().size)
-        .map { columIndex -> column(columIndex) }
-        .toTypedArray()
-
+fun Array<Int>.distinctValidNumbers() = this
+    .filter { "[1-9]".toRegex().matches(it.toString()) }
+    .toSet()
+    .count()
